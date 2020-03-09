823c3dd2d4778cad86843767ec147a89b7720196
Oscar Querevalu <oscarquerevalu@gmail.com>
2020-03-09 01:23:30 -0500

    Actualizacion Junit

---
+++ sysadap/src/test/java/pe/edu/upc/sysadap/spring/security/web/AlumnoControllerTest.java
@@ -0,0 +1,42 @@
+package pe.edu.upc.sysadap.spring.security.web;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+public class AlumnoControllerTest extends SpringBootHelloWorldTests{
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Test
+	public void testGetClaseAlumnos() throws Exception {
+		mockMvc.perform(get("/alumno/getDataClaseAlumnos")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"));
+
+	}
+	
+	@Test
+	public void testListAlumnos() throws Exception {
+		mockMvc.perform(get("/alumno/listAlumnos")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"));
+
+	}
+}
